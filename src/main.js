9ac2e90c6222e8dffcb5e0f65f3a4da52b536d31
Jkim1647 <kjy164777@gmail.com>
2026-07-15 16:10:51 +0900

    fix: load Leaflet marker icons correctly in production

--- src/main.js
+++ src/main.js
@@ -4,9 +4,20 @@ import router from './router'
 import App from './App.vue'
 import './assets/styles/main.css'
 import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png?url'
+import markerIcon from 'leaflet/dist/images/marker-icon.png?url'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png?url'
 import { initUiPreferences } from './composables/useUiPreferences'
 
 initUiPreferences()
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+	iconRetinaUrl: markerIcon2x,
+	iconUrl: markerIcon,
+	shadowUrl: markerShadow,
+})
 
 const app = createApp(App)
 
